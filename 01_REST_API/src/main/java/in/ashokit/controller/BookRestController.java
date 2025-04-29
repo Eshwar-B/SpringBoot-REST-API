784e51c48cca8d0ff9784ceede43a082f1536dd1
Eshwar-B <1728eshwar@gmail.com>
2025-04-29 11:54:08 +0530

    HTTP methods commit

--- 01_REST_API/src/main/java/in/ashokit/controller/BookRestController.java
+++ 01_REST_API/src/main/java/in/ashokit/controller/BookRestController.java
@@ -16,14 +16,13 @@ public class BookRestController {
 	@GetMapping(value = "/getbook", produces = "application/json")
 	public ResponseEntity<List<Book>> getBook()
 	{
-		Book b1 = new Book(01, "Brahmacharya", 350.50);
-		Book b2 = new Book(01, "Bhagavad Gita", 350.50);
-		Book b3 = new Book(01, "Ramayana", 350.50);
-		Book b4 = new Book(01, "Rig Veda", 350.50);
+		Book b1 = new Book(01, "rich dad, poor dad", 350.50);
+		Book b2 = new Book(02, "48 laws of power", 350.50);
+		Book b3 = new Book(03, "Engineering Mathematics", 350.50);
+		Book b4 = new Book(04, "Java 9th Edition", 350.50);
 		
 		List<Book> b = Arrays.asList(b1, b2, b3, b4);
 		
-//		return new ResponseEntity<>(b, HttpStatus.OK);
 		return ResponseEntity.ok(b);
 	}
 	
